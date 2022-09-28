252f76cb64ff42680151a79028fb7a5be376f984
Jorge Rodriguez <jorge.rodriguez@guayama.inter.edu>
2022-09-28 09:01:24 -0400

    Added jumping

--- Player.gd
+++ Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 export (int) var speed = 200 # pixels / second
 export (int) var GRAVITY = 800 # pixels / second / second
+export (int) var jump_force = 300
 
 onready var sprite = $Sprite
 
@@ -26,7 +27,15 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("walk_right"):
 		velocity.x = velocity.x + speed
 		sprite.flip_h = true
+	
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = -jump_force
 		
 	velocity.y = velocity.y + GRAVITY * delta
 		
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func _on_IdleTimer_timeout():
+	pass # Replace with function body.
