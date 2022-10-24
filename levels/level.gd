a4c5943f64cdd349ee6d9fa1ee91a30bf6c9b93d
Jorge Rodriguez <jlrm01@gmail.com>
2022-10-23 21:55:44 -0400

    added lights

--- levels/level.gd
+++ levels/level.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 onready var coin_bag_label = $"%CoinBagLabel"
+onready var canvas_layer: CanvasLayer = $"%CanvasLayer"
 
 
 func _ready():
-	pass
+	if canvas_layer:
+		canvas_layer.visible = true
 
 func _process(delta: float) -> void:
 	coin_bag_label.text = str(Global.player_coins)
